0e66797f30644f12d8e4b529f1a491d1f130c43a
Shirshendu Mukherjee <shirshendu.mukherjee.88@gmail.com>
2013-08-09 20:21:02 +0530

    changing test case to weakness of closest first algo

--- test/fully_observable_test.rb
+++ test/fully_observable_test.rb
@@ -1,5 +1,6 @@
 require './lib/botclean/fully_observable/environment.rb'
 require './lib/botclean/fully_observable/bot.rb'
+require 'pry'
 #Environment.data = "0 0
 #b---d
 #-d--d
@@ -7,12 +8,17 @@ require './lib/botclean/fully_observable/bot.rb'
 #--d--
 #----d"
 Botclean::FullyObservable::Environment.data = "0 0
-bd---
+bd--d
 -d---
 ---d-
 ---d-
---d-d"
+d-d--"
 #binding.pry
+  puts "Best path:"
+  best_path = Botclean::FullyObservable::Bot.find_path 0, 0, 5, 5, Botclean::FullyObservable::Environment.board
+  puts best_path.steps
+  puts best_path.steps.count
+  puts "================="
 count = 0
 until Botclean::FullyObservable::Environment.fully_clean? or count == 51 do
   action = Botclean::FullyObservable::Bot.next_move(Botclean::FullyObservable::Environment.robot_pos.c, Botclean::FullyObservable::Environment.robot_pos.r, 5, 5, Botclean::FullyObservable::Environment.board)
